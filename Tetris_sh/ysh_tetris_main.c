4fbbd1d3e803873f54ed70409480346f71b7f569
ysh <happysh_s2@naver.com>
2022-02-12 21:19:31 +0900

    Add code review for the current state

--- Tetris_sh/ysh_tetris_main.c
+++ Tetris_sh/ysh_tetris_main.c
@@ -3,6 +3,7 @@
 #include "ysh_tetris_Screen.h"
 int main(void) {
 	system("mode con cols=150 lines=40 | title tetris_양승현");
+	//지금까지의 작업은 천장까지 고려한 구현이다. 하지만 블록이 딱 한개 바닥에 쌓이면 종료된다.
 	tetris_process();
 
 	return 0;
