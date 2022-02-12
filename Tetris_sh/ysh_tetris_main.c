8f8c80e0403aa5f1459af4afe6befc0cafdf07e3
ysh <happysh_s2@naver.com>
2022-02-12 20:52:15 +0900

    Block go down, but do not move left or right.

--- Tetris_sh/ysh_tetris_main.c
+++ Tetris_sh/ysh_tetris_main.c
@@ -1,7 +1,9 @@
-#include "stdio.h"
-
+#include "ysh_tetris_blocks.h"
+#include "ysh_tetris_controller.h"
+#include "ysh_tetris_Screen.h"
 int main(void) {
 	system("mode con cols=150 lines=40 | title tetris_¾ç½ÂÇö");
+	tetris_process();
 
 	return 0;
 }
