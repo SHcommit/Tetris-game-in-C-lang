d383b3d3ee57dcef6bf11d67e15da4079c12888f
ysh <happysh_s2@naver.com>
2022-02-12 22:42:27 +0900

    Move consloe cursor func to tetris_consoleCursor source

--- Tetris_sh/tetris_controller.c
+++ Tetris_sh/tetris_controller.c
@@ -1,25 +1,5 @@
 #include "ysh_tetris_controller.h"
 #include "ysh_tetris_Screen.h"
-void GotoXY(int x, int y) { //커서를 원하고자하는 좌표로 이동,
-    COORD Pos;
-    Pos.X = x;
-    Pos.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
-}
-COORD getCursor(void) { //현재 커서 위치를 반환
-    COORD Pos;
-    CONSOLE_SCREEN_BUFFER_INFO curInfo;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
-    Pos.X = curInfo.dwCursorPosition.X;
-    Pos.Y = curInfo.dwCursorPosition.Y;
-    return Pos;
-}
-void CursorView(boolean show) {
-    CONSOLE_CURSOR_INFO ConsoleCursor;
-    ConsoleCursor.bVisible = show;
-    ConsoleCursor.dwSize = 10;
-    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &ConsoleCursor);
-}
 void BlockRotate() {
     turn = ++turn;
     if (turn == 4)
