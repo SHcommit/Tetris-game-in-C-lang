ac22bad2fd22076599714a49007f7042d90afcd0
ysh <happysh_s2@naver.com>
2022-02-12 20:47:33 +0900

    Add SPACEBAR define, IsMaxLine(),IsOverHeight() method. Delete ActionListener()

--- Tetris_sh/ysh_tetris_controller.h
+++ Tetris_sh/ysh_tetris_controller.h
@@ -13,6 +13,7 @@
 #define UP 72
 #define DOWN 80
 #define ARROW 224
+#define SPACEBAR 32
 #define Board_Width 14
 #define Board_Height 22
 #define Blocks_SIZE 4
@@ -21,7 +22,7 @@
 #define BlockStartY 4
 #define BoardX 4
 #define BoardY 3
-
+bool IsMaxLine();
 typedef enum gameSpeed { hard = 500, normal = 650, easy = 800 }SPEED;
 int BlockCOL;
 int BlockROW;//블록의 현재 좌표!!!
@@ -34,12 +35,13 @@ void tetris_process();
 COORD getCursor(void); //현재 커서 좌표 반환 
 void GotoXY(int x, int y);
 void CursorView(boolean show);
-void actoinListener();
 void BlockRotate(); //방향키 받을시 rotate 를통해 방향키 변경
 void CurrentShape();
 void NextShape();
 void IsBlockDropped();
 void deleteLine();
+boolean IsMaxLine();
+boolean IsOverHeight();
 boolean IsCollision(int shape, int rotate);
 //void addBlock(int Boards[][Board_Height],int currentBlocks[][Blocks_SIZE]);
 #endif // !controller
