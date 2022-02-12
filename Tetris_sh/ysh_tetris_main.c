008d5f03c94bc23fde0832b04d5179aec3d7a174
ysh <happysh_s2@naver.com>
2022-02-12 22:17:52 +0900

    Add func. almost complete.

--- Tetris_sh/ysh_tetris_main.c
+++ Tetris_sh/ysh_tetris_main.c
@@ -1,10 +1,15 @@
 #include "ysh_tetris_blocks.h"
 #include "ysh_tetris_controller.h"
 #include "ysh_tetris_Screen.h"
+#include <mmsystem.h>
+#pragma comment(lib,"winmm.lib")
 int main(void) {
-	system("mode con cols=150 lines=40 | title tetris_양승현");
-	//지금까지의 작업은 천장까지 고려한 구현이다. 하지만 블록이 딱 한개 바닥에 쌓이면 종료된다.
-	tetris_process();
+	CursorView(false);
+	system("mode con cols=110 lines=35 | title tetris_양승현");
+	PlaySound(TEXT("Runaway.wav"), NULL, SND_ASYNC | SND_LOOP);
 
+	GameTitle();
+	tetris_process();
+	InGameFrame();
 	return 0;
 }
