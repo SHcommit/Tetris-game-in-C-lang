c1aba609370dae00ff167afaf2bb487d8e39a4af
ysh <happysh_s2@naver.com>
2022-02-12 22:34:22 +0900

    Delete PlaySound func

--- Tetris_sh/ysh_tetris_main.c
+++ Tetris_sh/ysh_tetris_main.c
@@ -6,8 +6,6 @@
 int main(void) {
 	CursorView(false);
 	system("mode con cols=110 lines=35 | title tetris_¾ç½ÂÇö");
-	//PlaySound(TEXT("Runaway.wav"), NULL, SND_ASYNC | SND_LOOP);
-
 	GameTitle();
 	tetris_process();
 	InGameFrame();
