5a7b48c8af2ab4d4e4dde246fd56615fed704ed0
ysh <happysh_s2@naver.com>
2022-02-12 18:41:02 +0900

    Delcate breateBoards(), createGameTitle(), addBlock(), deleteBlock(), printBoards(), textcolor() method and boards array

--- Tetris_sh/ysh_tetris_Screen.h
+++ Tetris_sh/ysh_tetris_Screen.h
@@ -1,1 +1,17 @@
+#include "ysh_tetris_blocks.h"
+#include "ysh_tetris_controller.h"
 #pragma once
+
+#ifndef Frame
+#define Frame
+int static Rotation = 0;
+void createGameTitle();
+void createBoards();
+void addBlock(int shape, int rotate);
+void deleteBlock();
+int boards[Board_Height][Board_Width];
+void printBoards();
+void textcolor(int color_number);
+void IsBottemFull();
+
+#endif
