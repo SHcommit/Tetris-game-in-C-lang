29f62c4cacf1a3a15ab62a7673baf5975aa62126
ysh <happysh_s2@naver.com>
2022-02-12 22:12:05 +0900

    Declare bachground method

--- Tetris_sh/ysh_tetris_Screen.h
+++ Tetris_sh/ysh_tetris_Screen.h
@@ -6,14 +6,16 @@
 #define Frame
 int static level;
 void InGameFrame();
+void ShowNextBlock();
+void DeleteNextBlock();
 int static Rotation = 0;
-void createGameTitle();
+void GameTitle();
 void createBoards();
 void addBlock(int shape, int rotate);
 void deleteBlock();
 int boards[Board_Height][Board_Width];
+int NextBlock[6][6]; //nextshape
 void printBoards();
 void textcolor(int color_number);
 typedef enum Color { DARKPURPLE = 5, GRAY = 8, BLUE = 9, GREEN = 10, AQUA = 11, RED = 12, PURPLE = 13, YELLOW = 14, WHITE = 15 }COLOR;
-
 #endif
